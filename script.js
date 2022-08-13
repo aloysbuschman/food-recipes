b8694269b8023df67f8dd2493900f6cc2b359621
MarkoFilipovic27 <markofilipo@hotmail.com>
2022-08-13 19:41:54 +0200

    new css

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 // Functie om lijst met gerechten op te halen 
+async function getData(){
+  await fetch('https://themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+    });
+}
 
-fetch(`www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data)
-  });
 
-
+getData();
