a9225d0136b727fa59992aa081841e91f634364a
aloysbuschman <aloys.buschman@gmail.com>
2022-08-13 23:55:37 +0200

    Add modal html css and js

--- script.js
+++ script.js
@@ -3,8 +3,17 @@
 const mealWrapper = document.querySelector('.meal-wrapper');
 const submit1 = document.querySelector('.ingredient1-submit');
 const input = document.querySelector('#ingredient1');
+const selectMeal = document.querySelectorAll('.meal');
+const modal = document.querySelector('.modal-outer')
 
+//Event listners
 submit1.addEventListener('click', getMeals);
+
+
+function openModal(e) {
+  console.log(e.target.closest('.meal'));
+  modal.classList.toggle('show');
+}
 
 function getMeals(event) {
   event.preventDefault();
@@ -17,17 +26,18 @@ function getMeals(event) {
     let html = ``
     data.meals.forEach(meal => {
       html += `
-        <div class="meal">
+        <div class="meal" id="${meal.idMeal}">
           <div class="meal-image">
             <img src="${meal.strMealThumb}">
           </div>
         <div class="meal-info">
-          <h1>${meal.strMeal}</h1>
           <span class="meal-category">Work in progress</span>
-          <span class="meal-area">Work in progress</span> 
+          <span class="meal-area">Work in progress</span>
+          <h1 class="meal-title">${meal.strMeal}</h1>
         </div>
       </div>
       `
+      addEventListener('click', openModal);
     })
     mealWrapper.innerHTML = html;
   });
