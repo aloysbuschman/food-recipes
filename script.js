9a68957ac089a27efeb1d8c3cdbf6dd594a3e061
Aloys <62596372+aloysbuschman@users.noreply.github.com>
2022-08-13 23:48:27 +0200

    Add openModal draft

--- script.js
+++ script.js
@@ -3,8 +3,17 @@
 const mealWrapper = document.querySelector('.meal-wrapper');
 const submit1 = document.querySelector('.ingredient1-submit');
 const input = document.querySelector('#ingredient1');
+const selectMeal = document.querySelectorAll('.meal');
+const modal = document.querySelector('.modal-outer')
 
+//Event listners
 submit1.addEventListener('click', getMeals);
+
+
+function openModal(e) {
+  console.log(e.target.closest('.meal'));
+  modal.classList.toggle('show');
+}
 
 function getMeals(event) {
   event.preventDefault();
@@ -17,7 +26,7 @@ function getMeals(event) {
     let html = ``
     data.meals.forEach(meal => {
       html += `
-        <div class="meal">
+        <div class="meal" id="${meal.idMeal}">
           <div class="meal-image">
             <img src="${meal.strMealThumb}">
           </div>
@@ -28,6 +37,7 @@ function getMeals(event) {
         </div>
       </div>
       `
+      addEventListener('click', openModal);
     })
     mealWrapper.innerHTML = html;
   });
