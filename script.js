e91b9eda372ab8b7f1249fdde06dd6a4a9ff71fc
aloysbuschman <aloys.buschman@gmail.com>
2022-08-13 19:31:53 +0200

    API added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // Functie om lijst met gerechten op te halen 
 
-fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
+fetch(`www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`)
   .then((response) => response.json())
   .then((data) => {
     console.log(data)
