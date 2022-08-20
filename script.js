dac60a72b0e532aa873578fd2590c79fbd0c7268
aloysbuschman <aloys.buschman@gmail.com>
2022-08-20 12:14:37 +0200

    Write new logic for opening/closing the modal

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const mealWrapper = document.querySelector('.meal-wrapper');
 const submitBtn = document.querySelector('.ingredient1-submit');
 const inputField = document.querySelector('#ingredient1');
 const selectMeal = document.querySelectorAll('.meal');
-const modal = document.querySelector('.modal-outer')
+const modal = document.querySelector('.modal-outer');
+const closeModalBtn = document.querySelector('.close-modal');
 
 //Event listners
 submitBtn.addEventListener('click', getMeals);
+closeModalBtn.addEventListener('click', closeModal);
 
 // Functions 
 
@@ -25,13 +27,13 @@ function getMeals(event) {
         <div class="meal" data-id="${meal.idMeal}">
           <div class="meal-image">
             <img src="${meal.strMealThumb}">
-          </div>
+        </div>
         <div class="meal-info">
           <span class="meal-category">Work in progress</span>
           <span class="meal-area">Work in progress</span>
           <h1 class="meal-title">${meal.strMeal}</h1>
+          </div>
         </div>
-      </div>
       `
       addEventListener('click', openModal);
     })
@@ -41,5 +43,11 @@ function getMeals(event) {
 
 function openModal(e) {
   console.log(e.target.closest('.meal'));
-  modal.classList.toggle('show');
+  if (e.target.closest('.meal')) {
+    modal.classList.toggle('show');
+  }
+}
+
+function closeModal(e) {
+    modal.classList.toggle('show');
 }
